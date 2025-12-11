cb7ad409aa8727772f2c3656cac5e52fbfd14165
ishwork <ishworkhadka70@gmail.com>
2025-12-11 23:24:42 +0200

    enhance: add button to navigate to first city in the dropdown

--- src/components/HomePageWeather.tsx
+++ src/components/HomePageWeather.tsx
@@ -71,20 +71,32 @@ const HomePageWeather = () => {
 
       {/* Dropdown to select popular cities in Finland and navigate to their weather pages */}
       <div className="flex flex-col items-center mb-6">
-        <span className="mb-2  dark:text-blue-300 font-semibold">
+        <span className="mb-2 dark:text-blue-300 font-semibold">
           See weather condition in popular cities in Finland
         </span>
-        <select
-          value={dropdownCity}
-          onChange={handleDropdownChange}
-          className="px-4 py-2 rounded border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-200"
-        >
-          {CITIES_OPTIONS.map((option) => (
-            <option key={option} value={option}>
-              {option}
-            </option>
-          ))}
-        </select>
+        <div className="flex flex-row gap-3 w-full justify-center">
+          <select
+            value={dropdownCity}
+            onChange={handleDropdownChange}
+            className="px-4 py-2 rounded border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-200"
+          >
+            {CITIES_OPTIONS.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+          <button
+            type="button"
+            className="px-4 py-2 bg-gradient-to-r from-green-600 to-blue-600 text-white rounded hover:bg-blue-700 transition"
+            onClick={() => {
+              setDropdownCity(CITIES_OPTIONS[0]);
+              router.push(`/city/${encodeURIComponent(CITIES_OPTIONS[0])}`);
+            }}
+          >
+            See for {CITIES_OPTIONS[0]}
+          </button>
+        </div>
       </div>
 
       <div className="text-center">
